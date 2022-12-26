e299a7bc6bd323bc10d14e417876c5c779e21bd6
Onkar Apte <onkar.22apte@gmail.com>
2022-12-26 15:50:53 -0800

    Updated

--- Day5/day5.py
+++ Day5/day5.py
@@ -23,3 +23,42 @@ for i in inputfile:
 
 for i in inputvals:
     print(inputvals)
+
+def reverse_first_i(lst, i):
+    # Check if the value of i is within the valid range
+    if i < 0 or i > len(lst):
+        raise ValueError("i is out of range")
+
+    # Reverse the first i elements of the list
+    lst[:i] = reversed(lst[:i])
+
+# stacks = [['n','z'],['d','c','m'],['p']]
+# inputvals = [[1,2,1],[3,1,3],[2,2,1],[1,1,2]]
+
+def suffling(a,b,c):
+    temp=[]
+    for i in range(a):
+        stacks[c-1].insert(0,stacks[b-1][0])
+        stacks[b-1].remove(stacks[b-1][0])
+    reverse_first_i(stacks[c-1],a)
+        
+
+
+def part2(a,b,c):
+    for i in range(a):
+        temp=[]
+        temp.insert(0,stacks[b-1][0])
+        stacks[b-1].remove(stacks[b-1][0])
+    
+    stacks[c-1] = temp + stacks[c-1]
+
+
+for i in inputvals:
+    suffling(i[0],i[1],i[2])
+
+# for i in inputvals:
+#     part2(i[0],i[1],i[2])
+
+print(stacks)
+for i in stacks:
+    print(i[0])

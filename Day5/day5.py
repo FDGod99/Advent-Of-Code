b5a2c44268d8f615340478ac1123a5eb94a094e8
Onkar Apte <onkar.22apte@gmail.com>
2022-12-26 16:49:13 -0800

    Updated day5 code

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,3 +1,25 @@
+# Function to reverse first i elements of a given list
+def reverse_first_i(lst, i):
+    if i < 0 or i > len(lst):
+        raise ValueError("i is out of range")
+    lst[:i] = reversed(lst[:i])
+
+# Function for part 1
+def suffling1(a,b,c):
+    temp=[]
+    for i in range(a):
+        stacks[c-1].insert(0,stacks[b-1][0])
+        stacks[b-1].remove(stacks[b-1][0])
+
+# Function for part 2
+def suffling2(a,b,c):
+    temp=[]
+    for i in range(a):
+        stacks[c-1].insert(0,stacks[b-1][0])
+        stacks[b-1].remove(stacks[b-1][0])
+    reverse_first_i(stacks[c-1],a)
+        
+# Processing data
 one=['R','C','H']
 two=['F','S','L','H','J','B']
 three=['Q','T','J','H','D','M','R']
@@ -19,46 +41,44 @@ for i in inputfile:
         empty.append(int(a[3]))
         empty.append(int(a[5]))
         inputvals.append(empty)
-        
 
+# Calling function to solve part 1
 for i in inputvals:
-    print(inputvals)
+    suffling1(i[0],i[1],i[2])
 
-def reverse_first_i(lst, i):
-    # Check if the value of i is within the valid range
-    if i < 0 or i > len(lst):
-        raise ValueError("i is out of range")
+# Priting answer for part 1
+print("Part1:")
+for i in stacks:
+    print(i[0])
+print("\n")
 
-    # Reverse the first i elements of the list
-    lst[:i] = reversed(lst[:i])
+# Processing data again since we changed it in part 1
+one=['R','C','H']
+two=['F','S','L','H','J','B']
+three=['Q','T','J','H','D','M','R']
+four=['J','B','Z','H','R','G','S']
+five=['B','C','D','T','Z','F','P','R']
+six=['G','C','H','T']
+seven=['L','W','P','B','Z','V','N','S']
+eight=['C','G','Q','J','R']
+nine=['S','F','P','H','R','T','D','L']
+stacks = [one,two,three,four,five,six,seven,eight,nine]
 
-# stacks = [['n','z'],['d','c','m'],['p']]
-# inputvals = [[1,2,1],[3,1,3],[2,2,1],[1,1,2]]
+inputfile = open("/Users/fdgod/Desktop/My Projects/Advent-Of-Code-2022/Day5/day5.txt")
+inputvals=[]
+for i in inputfile:
+    if i[0]=='m':
+        empty=[]
+        a = i.split(" ")
+        empty.append(int(a[1]))
+        empty.append(int(a[3]))
+        empty.append(int(a[5]))
+        inputvals.append(empty)
 
-def suffling(a,b,c):
-    temp=[]
-    for i in range(a):
-        stacks[c-1].insert(0,stacks[b-1][0])
-        stacks[b-1].remove(stacks[b-1][0])
-    reverse_first_i(stacks[c-1],a)
-        
-
-
-def part2(a,b,c):
-    for i in range(a):
-        temp=[]
-        temp.insert(0,stacks[b-1][0])
-        stacks[b-1].remove(stacks[b-1][0])
-    
-    stacks[c-1] = temp + stacks[c-1]
-
-
+# Calling function to solve part 2
 for i in inputvals:
-    suffling(i[0],i[1],i[2])
-
-# for i in inputvals:
-#     part2(i[0],i[1],i[2])
-
-print(stacks)
+    suffling2(i[0],i[1],i[2])
+# Printing Answer to part 2
+print("Part2:")
 for i in stacks:
     print(i[0])

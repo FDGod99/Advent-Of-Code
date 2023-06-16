e119d8d646b58e3e468fd39f64c1ca7933b2c438
Onkar Apte <onkar.22apte@gmail.com>
2023-06-16 08:04:29 -0700

    solved new

---
+++ 2015/day2_2015_1.py
@@ -0,0 +1,22 @@
+#Taking inputs
+inputfile = open("/Users/fdgod/Desktop/My Projects/Advent-Of-Code-2022/2015/day2_2015.txt", "r")
+
+#Creating Array from txt file
+input_array = []
+for i in inputfile:
+    input_array.append(i.split("x"))
+input_array[-1][-1] = input_array[-1][-1]+"\n"  #last array does not have character \n, so adding it because surfacearea calculating fucntion is dependant on that
+
+
+#Creating a Function to Calculate the SurfaceArea
+def surfacearea(arr):
+    length = int(arr[0])
+    width =  int(arr[1])
+    height = int(arr[2].split("\\")[0])
+    return((2*length*width + 2*width*height + 2*height*length)+min([length*width,width*height,height*length]))
+
+#Solving the problem
+sum = 0
+for x in input_array:
+    sum = sum + surfacearea(x)
+print(sum)
